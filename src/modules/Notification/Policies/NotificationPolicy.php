dfa4fec6ad6db52b96611e17a9935adc37bb17fa
Marcos Calitas <marcoscalitas@gmail.com>
2026-03-26 10:22:47 +0100

    remove Repositories, add Policies and Actions folders

---
+++ src/modules/Notification/Policies/NotificationPolicy.php
@@ -0,0 +1,16 @@
+<?php
+
+namespace Modules\Notification\Policies;
+
+use Modules\User\Models\User;
+
+class NotificationPolicy
+{
+    /**
+     * Create a new policy instance.
+     */
+    public function __construct()
+    {
+        //
+    }
+}
---
+++ src/modules/Permission/Policies/RolePolicy.php
@@ -0,0 +1,16 @@
+<?php
+
+namespace Modules\Permission\Policies;
+
+use Modules\User\Models\User;
+
+class RolePolicy
+{
+    /**
+     * Create a new policy instance.
+     */
+    public function __construct()
+    {
+        //
+    }
+}
