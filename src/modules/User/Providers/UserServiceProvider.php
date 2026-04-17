06741eaa3a9f7f29bd5a0294b970adbd291ff1db
Marcos Calitas <marcoscalitas@gmail.com>
2026-04-17 15:10:41 +0100

    feat: register user policy in UserServiceProvider

--- src/modules/User/Providers/UserServiceProvider.php
+++ src/modules/User/Providers/UserServiceProvider.php
@@ -2,8 +2,11 @@
 
 namespace Modules\User\Providers;
 
+use Illuminate\Support\Facades\Gate;
 use Illuminate\Support\Facades\Route;
 use Illuminate\Support\ServiceProvider;
+use Modules\User\Models\User;
+use Modules\User\Policies\UserPolicy;
 
 class UserServiceProvider extends ServiceProvider
 {
@@ -20,6 +23,8 @@ class UserServiceProvider extends ServiceProvider
      */
     public function boot(): void
     {
+        Gate::policy(User::class, UserPolicy::class);
+
         Route::middleware('web')->group(__DIR__ . '/../Routes/web.php');
         Route::prefix('api')->middleware('api')->group(__DIR__ . '/../Routes/api.php');
         $this->loadMigrationsFrom(__DIR__ . '/../Database/Migrations');
