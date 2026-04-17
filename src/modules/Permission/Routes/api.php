9a7cf7f7ba26acce4b3914779949db7d2fcf4a2f
Marcos Calitas <marcoscalitas@gmail.com>
2026-04-17 15:22:47 +0100

    fix: remove duplicated URL prefix in Permission API routes

--- src/modules/Permission/Routes/api.php
+++ src/modules/Permission/Routes/api.php
@@ -5,7 +5,7 @@ use Modules\Permission\Http\Controllers\PermissionController;
 use Modules\Permission\Http\Controllers\RoleController;
 
 // Rate limiting: 60 requests/minute for authenticated users
-Route::middleware(['auth:api', 'throttle:60,1'])->prefix('permissions')->group(function () {
+Route::middleware(['auth:api', 'throttle:60,1'])->group(function () {
     Route::apiResource('roles', RoleController::class);
     Route::apiResource('permissions', PermissionController::class);
 });
