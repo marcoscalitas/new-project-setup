8684ef539e247dcd3d348218c7d39f1d653bdb82
Marcos Calitas <marcoscalitas@gmail.com>
2026-04-17 15:30:13 +0100

    refactor: move Auth event listeners to AuthServiceProvider

--- src/modules/Auth/Providers/AuthServiceProvider.php
+++ src/modules/Auth/Providers/AuthServiceProvider.php
@@ -2,9 +2,13 @@
 
 namespace Modules\Auth\Providers;
 
+use Illuminate\Support\Facades\Event;
 use Illuminate\Support\Facades\Route;
 use Illuminate\Support\ServiceProvider;
 use Laravel\Passport\Passport;
+use Modules\Auth\Events\UserCreated;
+use Modules\Auth\Listeners\LogUserCreation;
+use Modules\Auth\Listeners\SendWelcomeEmail;
 
 class AuthServiceProvider extends ServiceProvider
 {
@@ -21,6 +25,9 @@ class AuthServiceProvider extends ServiceProvider
      */
     public function boot(): void
     {
+        Event::listen(UserCreated::class, [SendWelcomeEmail::class, 'handle']);
+        Event::listen(UserCreated::class, [LogUserCreation::class, 'handle']);
+
         Route::middleware('web')->group(__DIR__ . '/../Routes/web.php');
         Route::prefix('api')->middleware('api')->group(__DIR__ . '/../Routes/api.php');
         $this->loadMigrationsFrom(__DIR__ . '/../Database/Migrations');
