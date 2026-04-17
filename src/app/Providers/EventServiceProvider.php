7748ff298f698646754f6e3c9a2a3cfa9bbd80ac
Marcos Calitas <marcoscalitas@gmail.com>
2026-04-17 15:30:13 +0100

    refactor: remove module event mappings from central EventServiceProvider

--- src/app/Providers/EventServiceProvider.php
+++ src/app/Providers/EventServiceProvider.php
@@ -5,25 +5,6 @@ namespace App\Providers;
 use Illuminate\Auth\Events\Registered;
 use Illuminate\Auth\Listeners\SendEmailVerificationNotification;
 use Illuminate\Foundation\Support\Providers\EventServiceProvider as ServiceProvider;
-use Modules\Auth\Events\UserCreated;
-use Modules\Auth\Listeners\SendWelcomeEmail;
-use Modules\Auth\Listeners\LogUserCreation;
-use Modules\Notification\Events\NotificationDeleted;
-use Modules\Notification\Events\NotificationRead;
-use Modules\Notification\Listeners\LogNotificationDeletion;
-use Modules\Notification\Listeners\LogNotificationRead;
-use Modules\Permission\Events\PermissionCreated;
-use Modules\Permission\Events\PermissionDeleted;
-use Modules\Permission\Events\PermissionUpdated;
-use Modules\Permission\Events\RoleAssigned;
-use Modules\Permission\Listeners\LogPermissionCreation;
-use Modules\Permission\Listeners\LogPermissionDeletion;
-use Modules\Permission\Listeners\LogPermissionUpdate;
-use Modules\Permission\Listeners\LogRoleChange;
-use Modules\User\Events\UserDeleted;
-use Modules\User\Events\UserUpdated;
-use Modules\User\Listeners\LogUserDeletion;
-use Modules\User\Listeners\LogUserUpdate;
 
 class EventServiceProvider extends ServiceProvider
 {
@@ -35,34 +16,6 @@ class EventServiceProvider extends ServiceProvider
     protected $listen = [
         Registered::class => [
             SendEmailVerificationNotification::class,
-        ],
-        UserCreated::class => [
-            SendWelcomeEmail::class,
-            LogUserCreation::class,
-        ],
-        UserUpdated::class => [
-            LogUserUpdate::class,
-        ],
-        UserDeleted::class => [
-            LogUserDeletion::class,
-        ],
-        RoleAssigned::class => [
-            LogRoleChange::class,
-        ],
-        PermissionCreated::class => [
-            LogPermissionCreation::class,
-        ],
-        PermissionUpdated::class => [
-            LogPermissionUpdate::class,
-        ],
-        PermissionDeleted::class => [
-            LogPermissionDeletion::class,
-        ],
-        NotificationRead::class => [
-            LogNotificationRead::class,
-        ],
-        NotificationDeleted::class => [
-            LogNotificationDeletion::class,
         ],
     ];
 
