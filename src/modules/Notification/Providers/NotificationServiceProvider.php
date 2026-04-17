3511d9238644c36586143c5056b5e78cf5efb82d
Marcos Calitas <marcoscalitas@gmail.com>
2026-04-17 15:30:13 +0100

    refactor: move Notification event listeners to NotificationServiceProvider

--- src/modules/Notification/Providers/NotificationServiceProvider.php
+++ src/modules/Notification/Providers/NotificationServiceProvider.php
@@ -2,8 +2,13 @@
 
 namespace Modules\Notification\Providers;
 
+use Illuminate\Support\Facades\Event;
 use Illuminate\Support\Facades\Route;
 use Illuminate\Support\ServiceProvider;
+use Modules\Notification\Events\NotificationDeleted;
+use Modules\Notification\Events\NotificationRead;
+use Modules\Notification\Listeners\LogNotificationDeletion;
+use Modules\Notification\Listeners\LogNotificationRead;
 
 class NotificationServiceProvider extends ServiceProvider
 {
@@ -20,6 +25,9 @@ class NotificationServiceProvider extends ServiceProvider
      */
     public function boot(): void
     {
+        Event::listen(NotificationRead::class, [LogNotificationRead::class, 'handle']);
+        Event::listen(NotificationDeleted::class, [LogNotificationDeletion::class, 'handle']);
+
         Route::middleware('web')->group(__DIR__ . '/../Routes/web.php');
         Route::prefix('api')->middleware('api')->group(__DIR__ . '/../Routes/api.php');
         $this->loadMigrationsFrom(__DIR__ . '/../Database/Migrations');
