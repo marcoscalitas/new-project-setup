4c9bf79ddeb06ca71905cef20fb7aa92d30b22bb
Marcos Calitas <marcoscalitas@gmail.com>
2026-04-17 15:30:13 +0100

    refactor: move Permission event listeners to PermissionServiceProvider

--- src/modules/Permission/Providers/PermissionServiceProvider.php
+++ src/modules/Permission/Providers/PermissionServiceProvider.php
@@ -2,9 +2,18 @@
 
 namespace Modules\Permission\Providers;
 
+use Illuminate\Support\Facades\Event;
 use Illuminate\Support\Facades\Gate;
 use Illuminate\Support\Facades\Route;
 use Illuminate\Support\ServiceProvider;
+use Modules\Permission\Events\PermissionCreated;
+use Modules\Permission\Events\PermissionDeleted;
+use Modules\Permission\Events\PermissionUpdated;
+use Modules\Permission\Events\RoleAssigned;
+use Modules\Permission\Listeners\LogPermissionCreation;
+use Modules\Permission\Listeners\LogPermissionDeletion;
+use Modules\Permission\Listeners\LogPermissionUpdate;
+use Modules\Permission\Listeners\LogRoleChange;
 use Modules\Permission\Models\Permission;
 use Modules\Permission\Models\Role;
 use Modules\Permission\Policies\PermissionPolicy;
@@ -27,6 +36,11 @@ class PermissionServiceProvider extends ServiceProvider
     {
         Gate::policy(Permission::class, PermissionPolicy::class);
         Gate::policy(Role::class, RolePolicy::class);
+
+        Event::listen(RoleAssigned::class, [LogRoleChange::class, 'handle']);
+        Event::listen(PermissionCreated::class, [LogPermissionCreation::class, 'handle']);
+        Event::listen(PermissionUpdated::class, [LogPermissionUpdate::class, 'handle']);
+        Event::listen(PermissionDeleted::class, [LogPermissionDeletion::class, 'handle']);
 
         Route::middleware('web')->group(__DIR__ . '/../Routes/web.php');
         Route::prefix('api')->middleware('api')->group(__DIR__ . '/../Routes/api.php');
