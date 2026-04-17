11e64a5d478601355284f96b02a7943986d97516
Marcos Calitas <marcoscalitas@gmail.com>
2026-04-17 15:30:13 +0100

    refactor: move User event listeners to UserServiceProvider

--- src/modules/User/Providers/UserServiceProvider.php
+++ src/modules/User/Providers/UserServiceProvider.php
@@ -2,9 +2,14 @@
 
 namespace Modules\User\Providers;
 
+use Illuminate\Support\Facades\Event;
 use Illuminate\Support\Facades\Gate;
 use Illuminate\Support\Facades\Route;
 use Illuminate\Support\ServiceProvider;
+use Modules\User\Events\UserDeleted;
+use Modules\User\Events\UserUpdated;
+use Modules\User\Listeners\LogUserDeletion;
+use Modules\User\Listeners\LogUserUpdate;
 use Modules\User\Models\User;
 use Modules\User\Policies\UserPolicy;
 
@@ -24,6 +29,9 @@ class UserServiceProvider extends ServiceProvider
     public function boot(): void
     {
         Gate::policy(User::class, UserPolicy::class);
+
+        Event::listen(UserUpdated::class, [LogUserUpdate::class, 'handle']);
+        Event::listen(UserDeleted::class, [LogUserDeletion::class, 'handle']);
 
         Route::middleware('web')->group(__DIR__ . '/../Routes/web.php');
         Route::prefix('api')->middleware('api')->group(__DIR__ . '/../Routes/api.php');
