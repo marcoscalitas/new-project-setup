3c5cbeecbf3a61b43d0d03ead1e6300be3184ee5
Marcos Calitas <marcoscalitas@gmail.com>
2026-04-17 15:22:47 +0100

    fix: remove duplicated URL prefix in Permission web routes

--- src/modules/Permission/Routes/web.php
+++ src/modules/Permission/Routes/web.php
@@ -4,18 +4,20 @@ use Illuminate\Support\Facades\Route;
 use Modules\Permission\Http\Controllers\RoleController;
 use Modules\Permission\Http\Controllers\PermissionController;
 
-Route::prefix('permissions')
-    ->middleware(['auth', 'throttle:60,1'])
-    ->group(function () {
-        Route::get('/roles',            [RoleController::class, 'index'])->name('roles.index');
-        Route::post('/roles',           [RoleController::class, 'store'])->name('roles.store');
-        Route::get('/roles/{id}',       [RoleController::class, 'show'])->name('roles.show');
-        Route::put('/roles/{id}',       [RoleController::class, 'update'])->name('roles.update');
-        Route::delete('/roles/{id}',    [RoleController::class, 'destroy'])->name('roles.destroy');
-
-        Route::get('/permissions',            [PermissionController::class, 'index'])->name('permissions.index');
-        Route::post('/permissions',           [PermissionController::class, 'store'])->name('permissions.store');
-        Route::get('/permissions/{id}',       [PermissionController::class, 'show'])->name('permissions.show');
-        Route::put('/permissions/{id}',       [PermissionController::class, 'update'])->name('permissions.update');
-        Route::delete('/permissions/{id}',    [PermissionController::class, 'destroy'])->name('permissions.destroy');
+Route::middleware(['auth', 'throttle:60,1'])->group(function () {
+    Route::prefix('roles')->group(function () {
+        Route::get('/',        [RoleController::class, 'index'])->name('roles.index');
+        Route::post('/',       [RoleController::class, 'store'])->name('roles.store');
+        Route::get('/{id}',    [RoleController::class, 'show'])->name('roles.show');
+        Route::put('/{id}',    [RoleController::class, 'update'])->name('roles.update');
+        Route::delete('/{id}', [RoleController::class, 'destroy'])->name('roles.destroy');
     });
+
+    Route::prefix('permissions')->group(function () {
+        Route::get('/',        [PermissionController::class, 'index'])->name('permissions.index');
+        Route::post('/',       [PermissionController::class, 'store'])->name('permissions.store');
+        Route::get('/{id}',    [PermissionController::class, 'show'])->name('permissions.show');
+        Route::put('/{id}',    [PermissionController::class, 'update'])->name('permissions.update');
+        Route::delete('/{id}', [PermissionController::class, 'destroy'])->name('permissions.destroy');
+    });
+});
