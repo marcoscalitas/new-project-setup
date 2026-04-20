c850f418b419860a52633447ddf7d17c551625e9
Marcos Calitas <marcoscalitas@gmail.com>
2026-04-20 09:09:22 +0100

    feat: register Role event listeners in PermissionServiceProvider

--- src/modules/Permission/Providers/PermissionServiceProvider.php
+++ src/modules/Permission/Providers/PermissionServiceProvider.php
@@ -10,10 +10,16 @@ use Modules\Permission\Events\PermissionCreated;
 use Modules\Permission\Events\PermissionDeleted;
 use Modules\Permission\Events\PermissionUpdated;
 use Modules\Permission\Events\RoleAssigned;
+use Modules\Permission\Events\RoleCreated;
+use Modules\Permission\Events\RoleDeleted;
+use Modules\Permission\Events\RoleUpdated;
 use Modules\Permission\Listeners\LogPermissionCreation;
 use Modules\Permission\Listeners\LogPermissionDeletion;
 use Modules\Permission\Listeners\LogPermissionUpdate;
 use Modules\Permission\Listeners\LogRoleChange;
+use Modules\Permission\Listeners\LogRoleCreation;
+use Modules\Permission\Listeners\LogRoleDeletion;
+use Modules\Permission\Listeners\LogRoleUpdate;
 use Modules\Permission\Models\Permission;
 use Modules\Permission\Models\Role;
 use Modules\Permission\Policies\PermissionPolicy;
@@ -38,6 +44,9 @@ class PermissionServiceProvider extends ServiceProvider
         Gate::policy(Role::class, RolePolicy::class);
 
         Event::listen(RoleAssigned::class, [LogRoleChange::class, 'handle']);
+        Event::listen(RoleCreated::class, [LogRoleCreation::class, 'handle']);
+        Event::listen(RoleUpdated::class, [LogRoleUpdate::class, 'handle']);
+        Event::listen(RoleDeleted::class, [LogRoleDeletion::class, 'handle']);
         Event::listen(PermissionCreated::class, [LogPermissionCreation::class, 'handle']);
         Event::listen(PermissionUpdated::class, [LogPermissionUpdate::class, 'handle']);
         Event::listen(PermissionDeleted::class, [LogPermissionDeletion::class, 'handle']);
