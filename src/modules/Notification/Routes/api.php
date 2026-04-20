7534db493a7a0696ddcb7a609d5a95e95626c727
Marcos Calitas <marcoscalitas@gmail.com>
2026-04-20 08:34:39 +0100

    feat: add api. prefix and names to Notification API routes

--- src/modules/Notification/Routes/api.php
+++ src/modules/Notification/Routes/api.php
@@ -5,10 +5,10 @@ use Modules\Notification\Http\Controllers\NotificationController;
 
 // Rate limiting: 60 requests/minute for authenticated users
 Route::middleware(['auth:api', 'throttle:60,1'])->prefix('notifications')->group(function () {
-    Route::get('/',          [NotificationController::class, 'index']);
-    Route::get('/unread',    [NotificationController::class, 'unread']);
-    Route::get('/{id}',      [NotificationController::class, 'show']);
-    Route::patch('/{id}/read', [NotificationController::class, 'markAsRead']);
-    Route::post('/read-all', [NotificationController::class, 'markAllAsRead']);
-    Route::delete('/{id}',   [NotificationController::class, 'destroy']);
+    Route::get('/',            [NotificationController::class, 'index'])->name('api.notifications.index');
+    Route::get('/unread',      [NotificationController::class, 'unread'])->name('api.notifications.unread');
+    Route::get('/{id}',        [NotificationController::class, 'show'])->name('api.notifications.show');
+    Route::patch('/{id}/read', [NotificationController::class, 'markAsRead'])->name('api.notifications.read');
+    Route::post('/read-all',   [NotificationController::class, 'markAllAsRead'])->name('api.notifications.readAll');
+    Route::delete('/{id}',     [NotificationController::class, 'destroy'])->name('api.notifications.destroy');
 });
