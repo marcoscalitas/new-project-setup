61966e2888c2520494d09a35808e0b3f9ad38fca
Marcos Calitas <marcoscalitas@gmail.com>
2026-04-20 09:29:04 +0100

    refactor: remove unused AuthController import from Auth web routes

--- src/modules/Auth/Routes/web.php
+++ src/modules/Auth/Routes/web.php
@@ -1,6 +1,5 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
-use Modules\Auth\Http\Controllers\AuthController;
 
 // Web auth routes are handled by Fortify (prefix 'auth' set in config/fortify.php)
