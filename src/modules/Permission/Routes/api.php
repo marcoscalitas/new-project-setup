92c48734af86837954ee862a703bcc0bb6266eca
Marcos Calitas <marcoscalitas@gmail.com>
2026-04-20 08:34:39 +0100

    feat: add api. prefix to Permission API route names

--- src/modules/Permission/Routes/api.php
+++ src/modules/Permission/Routes/api.php
@@ -6,6 +6,6 @@ use Modules\Permission\Http\Controllers\RoleController;
 
 // Rate limiting: 60 requests/minute for authenticated users
 Route::middleware(['auth:api', 'throttle:60,1'])->group(function () {
-    Route::apiResource('roles', RoleController::class);
-    Route::apiResource('permissions', PermissionController::class);
+    Route::apiResource('roles', RoleController::class)->names('api.roles');
+    Route::apiResource('permissions', PermissionController::class)->names('api.permissions');
 });
