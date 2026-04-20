9e31cce21a41ec9d21ee4ea97ce81d17389fe61a
Marcos Calitas <marcoscalitas@gmail.com>
2026-04-20 09:09:47 +0100

    feat: add ActivityNotification class for database notifications

---
+++ src/modules/Notification/Notifications/ActivityNotification.php
@@ -0,0 +1,33 @@
+<?php
+
+namespace Modules\Notification\Notifications;
+
+use Illuminate\Bus\Queueable;
+use Illuminate\Contracts\Queue\ShouldQueue;
+use Illuminate\Notifications\Notification;
+
+class ActivityNotification extends Notification implements ShouldQueue
+{
+    use Queueable;
+
+    public function __construct(
+        private string $type,
+        private string $message,
+        private array $data = [],
+    ) {
+    }
+
+    public function via(object $notifiable): array
+    {
+        return ['database'];
+    }
+
+    public function toArray(object $notifiable): array
+    {
+        return [
+            'type'    => $this->type,
+            'message' => $this->message,
+            'data'    => $this->data,
+        ];
+    }
+}
