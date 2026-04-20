c2857645297998abfd08f7b47838b314d7e349d9
Marcos Calitas <marcoscalitas@gmail.com>
2026-04-20 10:14:26 +0100

    feat: add dual response (JSON + Blade) to UserController

--- src/modules/User/Http/Controllers/UserController.php
+++ src/modules/User/Http/Controllers/UserController.php
@@ -9,53 +9,94 @@ use Modules\User\Http\Requests\UpdateUserRequest;
 use Modules\User\Http\Resources\UserResource;
 use Modules\User\Models\User;
 use Modules\User\Services\UserService;
+use Spatie\Permission\Models\Role;
 
 class UserController
 {
     public function __construct(private UserService $userService) {}
 
-    public function index(): JsonResponse
+    public function index(): JsonResponse|\Illuminate\View\View
     {
         Gate::authorize('viewAny', User::class);
 
         $users = $this->userService->getAll();
 
-        return response()->json(UserResource::collection($users));
+        if (request()->expectsJson()) {
+            return response()->json(UserResource::collection($users));
+        }
+
+        return view('user::users.index', compact('users'));
     }
 
-    public function store(StoreUserRequest $request): JsonResponse
+    public function create(): \Illuminate\View\View
+    {
+        Gate::authorize('create', User::class);
+
+        $roles = Role::where('guard_name', 'web')->get();
+
+        return view('user::users.create', compact('roles'));
+    }
+
+    public function store(StoreUserRequest $request): JsonResponse|\Illuminate\Http\RedirectResponse
     {
         Gate::authorize('create', User::class);
 
         $user = $this->userService->create($request->validated());
 
-        return response()->json(new UserResource($user), 201);
+        if (request()->expectsJson()) {
+            return response()->json(new UserResource($user), 201);
+        }
+
+        return redirect()->route('users.index')->with('success', 'User created.');
     }
 
-    public function show(int $id): JsonResponse
+    public function show(int $id): JsonResponse|\Illuminate\View\View
     {
         $user = $this->userService->findById($id);
 
         Gate::authorize('view', $user);
 
-        return response()->json(new UserResource($user));
+        if (request()->expectsJson()) {
+            return response()->json(new UserResource($user));
+        }
+
+        return view('user::users.show', compact('user'));
     }
 
-    public function update(UpdateUserRequest $request, int $id): JsonResponse
+    public function edit(int $id): \Illuminate\View\View
+    {
+        $user = User::findOrFail($id);
+
+        Gate::authorize('update', $user);
+
+        $roles = Role::where('guard_name', 'web')->get();
+
+        return view('user::users.edit', compact('user', 'roles'));
+    }
+
+    public function update(UpdateUserRequest $request, int $id): JsonResponse|\Illuminate\Http\RedirectResponse
     {
         Gate::authorize('update', User::findOrFail($id));
 
         $user = $this->userService->update($id, $request->validated());
 
-        return response()->json(new UserResource($user));
+        if (request()->expectsJson()) {
+            return response()->json(new UserResource($user));
+        }
+
+        return redirect()->route('users.index')->with('success', 'User updated.');
     }
 
-    public function destroy(int $id): JsonResponse
+    public function destroy(int $id): JsonResponse|\Illuminate\Http\RedirectResponse
     {
         Gate::authorize('delete', User::findOrFail($id));
 
         $this->userService->delete($id);
 
-        return response()->json(null, 204);
+        if (request()->expectsJson()) {
+            return response()->json(null, 204);
+        }
+
+        return redirect()->route('users.index')->with('success', 'User deleted.');
     }
 }
