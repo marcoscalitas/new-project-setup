a1ea189083b959c4fc6ded479055e098e1379286
Marcos Calitas <marcoscalitas@gmail.com>
2026-04-21 09:58:53 +0100

    feat: install Telescope for local debugging

--- src/app/Providers/AppServiceProvider.php
+++ src/app/Providers/AppServiceProvider.php
@@ -3,20 +3,18 @@
 namespace App\Providers;
 
 use Illuminate\Support\ServiceProvider;
+use Laravel\Telescope\TelescopeServiceProvider;
 
 class AppServiceProvider extends ServiceProvider
 {
-    /**
-     * Register any application services.
-     */
     public function register(): void
     {
-        //
+        if ($this->app->isLocal()) {
+            $this->app->register(TelescopeServiceProvider::class);
+            $this->app->register(\App\Providers\TelescopeServiceProvider::class);
+        }
     }
 
-    /**
-     * Bootstrap any application services.
-     */
     public function boot(): void
     {
         //
--- src/bootstrap/providers.php
+++ src/bootstrap/providers.php
@@ -2,6 +2,7 @@
 
 return [
     App\Providers\AppServiceProvider::class,
+    App\Providers\HorizonServiceProvider::class,
     Modules\Auth\Providers\AuthServiceProvider::class,
     Modules\Auth\Providers\FortifyServiceProvider::class,
     Modules\Notification\Providers\NotificationServiceProvider::class,
