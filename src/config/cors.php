0b3baebb351cdd5d0411811b96e48150ef111d04
Marcos Calitas <marcoscalitas@gmail.com>
2026-04-21 08:29:14 +0100

    feat: add explicit CORS config with CORS_ALLOWED_ORIGINS env variable

---
+++ src/config/cors.php
@@ -0,0 +1,36 @@
+<?php
+
+return [
+
+    /*
+    |--------------------------------------------------------------------------
+    | Cross-Origin Resource Sharing (CORS) Configuration
+    |--------------------------------------------------------------------------
+    |
+    | Here you may configure your settings for cross-origin resource sharing
+    | or "CORS". This determines what cross-origin operations may execute
+    | in web browsers. You are free to adjust these settings as needed.
+    |
+    | To learn more: https://developer.mozilla.org/en-US/docs/Web/HTTP/CORS
+    |
+    */
+
+    'paths' => ['api/*', 'sanctum/csrf-cookie'],
+
+    'allowed_methods' => ['GET', 'POST', 'PUT', 'PATCH', 'DELETE', 'OPTIONS'],
+
+    'allowed_origins' => array_filter(
+        explode(',', env('CORS_ALLOWED_ORIGINS', 'http://localhost:3000'))
+    ),
+
+    'allowed_origins_patterns' => [],
+
+    'allowed_headers' => ['Content-Type', 'Authorization', 'Accept', 'X-Requested-With'],
+
+    'exposed_headers' => ['X-RateLimit-Limit', 'X-RateLimit-Remaining'],
+
+    'max_age' => 86400,
+
+    'supports_credentials' => false,
+
+];
