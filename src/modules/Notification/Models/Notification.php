30aa8dd64a2d6aa81bd33f4883c47cfa098d06bb
Marcos Calitas <marcoscalitas@gmail.com>
2026-04-22 10:56:13 +0100

    feat: add LogsActivity trait to Notification model

--- src/modules/Notification/Models/Notification.php
+++ src/modules/Notification/Models/Notification.php
@@ -4,8 +4,20 @@ namespace Modules\Notification\Models;
 
 use Illuminate\Database\Eloquent\SoftDeletes;
 use Illuminate\Notifications\DatabaseNotification;
+use Spatie\Activitylog\Models\Concerns\LogsActivity;
+use Spatie\Activitylog\Support\LogOptions;
 
 class Notification extends DatabaseNotification
 {
-    use SoftDeletes;
+    use LogsActivity, SoftDeletes;
+
+    protected static $recordEvents = ['updated', 'deleted'];
+
+    public function getActivitylogOptions(): LogOptions
+    {
+        return LogOptions::defaults()
+            ->logOnly(['read_at'])
+            ->logOnlyDirty()
+            ->dontLogEmptyChanges();
+    }
 }
