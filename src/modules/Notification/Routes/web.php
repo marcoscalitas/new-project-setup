ca3c77009ab0f7dfbdbd2fb4c2fbc066731627d2
Marcos Calitas <marcoscalitas@gmail.com>
2026-04-30 10:09:23 +0100

    refactor: remove notification web routes

--- src/modules/Notification/Routes/web.php
+++
@@ -1,8 +0,0 @@
-<?php
-
-use Illuminate\Support\Facades\Route;
-use Modules\Notification\Http\Controllers\NotificationController;
-
-Route::middleware(['web', 'auth', 'verified'])->group(function () {
-    Route::post('/notifications/read-all', [NotificationController::class, 'markAllAsRead'])->name('notifications.readAll');
-});
