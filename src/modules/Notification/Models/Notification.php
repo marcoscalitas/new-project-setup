e2f59a07175e3885bb2e32f0494cecd77de009b3
Marcos Calitas <marcoscalitas@gmail.com>
2026-04-30 13:29:46 +0100

    fix: generate ULID on notification creation via booted hook

--- src/modules/Notification/Models/Notification.php
+++ src/modules/Notification/Models/Notification.php
@@ -4,6 +4,7 @@ namespace Modules\Notification\Models;
 
 use Illuminate\Database\Eloquent\SoftDeletes;
 use Illuminate\Notifications\DatabaseNotification;
+use Illuminate\Support\Str;
 use Spatie\Activitylog\Models\Concerns\LogsActivity;
 use Spatie\Activitylog\Support\LogOptions;
 
@@ -12,6 +13,13 @@ class Notification extends DatabaseNotification
     use LogsActivity, SoftDeletes;
 
     protected static $recordEvents = ['updated', 'deleted'];
+
+    protected static function booted(): void
+    {
+        static::creating(function (self $model) {
+            $model->id = (string) Str::ulid();
+        });
+    }
 
     public function getActivitylogOptions(): LogOptions
     {
